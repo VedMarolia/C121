bcf44492e4bc8d899ae5cad28d9d8e4b658793e5
VedMarolia <118649989+VedMarolia@users.noreply.github.com>
2023-08-27 10:47:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+previous_result = ""
+
+function setup() {
+    canvas = createCanvas(300, 300)
+    canvas.position(525, 300)
+    video = createCapture(VIDEO)
+    video.hide();
+    classifier = ml5.imageClassifier("MobileNet", modelLoaded)
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300)
+    classifier.classify(video, gotResults)
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+}
+
+function gotResults(error, result) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(result)
+
+        if ((result[0].confidence > 0.5) && (previous_result != result[0].label)) {
+
+            previous_result = result[0].label
+
+            document.getElementById("object").innerHTML = "Object : " + result[0].label
+            document.getElementById("accuracy").innerHTML = "Accuracy : " + (result[0].confidence * 100).toFixed(2)
+        }
+    }
+}
